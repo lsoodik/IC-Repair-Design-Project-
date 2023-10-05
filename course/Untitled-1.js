6611c7407dcd0da827a7e1f79a365c51d39358f8
sonya <ssonya2311@gmail.com>
2023-10-05 12:48:50 +0700

    first commit

---
+++ course/Untitled-1.js
@@ -0,0 +1,9 @@
+x=prompt("Введите число", "");
+console.log(typeof 43);
+// if ((x="Number") && (x%2===0))
+// if (x="Number")
+//     console.log("четное число")
+// else if((x="Number") && (x%2!==0))
+//     console.log("нечетное число")
+// else  (x!="Number")
+//     console.log("Кажется вы ошиблись")
